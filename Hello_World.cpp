1563fc2a8fd7e8c533a5de5fa562ce4044a11870
Mikolaj Fuglewicz <fuglewicz@mikolaj.com>
2018-10-16 14:15:34 +0200

    Poprawienie + dodanie zadania z hello wolrd v01

---
+++ Hello_World.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Hello World! What a beatiful day!" << std::endl;
+}
--- Lab2.cc
+++ Lab2.cc
@@ -2,12 +2,18 @@
 #include <iostream>
 using namespace std;
 
-//To jest komentaż do tego pliku. Ten plik jest ewidentnie źle fsormatowany i należy to koniecznie poprawić, pamiętając o nazwach zmiennych
-//dopuszczalnej długości linii, znakach z kodu ANSI, wcięciach i dbalości o wsykoiehj jakosci kometarze
+/*To jest komentarz do tego pliku.
+Ten plik jest ewidentnie źle sformatowany i należy to koniecznie poprawić,
+pamiętając o nazwach zmiennych dopuszczalnej długości linii, znakach z kodu
+ANSI, wcięciach i dbalości o wsykoiej jakosci kometarze*/
 
-int main() { int PierwszaZmienna=5; int Drugazmienna=7;
-int WynikDziałania=PierwszaZmienna+Drugazmienna;
+int main()
+{
+    int a = 5 ; //Pierwsza zmienna
+    int b = 7 ; //Druga zmienna
 
-cout <<"Wynik Dodawania Dwóch Liczb To:"<<WynikDziałania<<endl; // Wypisuje wynik dzialania
-return 0;
+
+    cout << "Wynik Dodawania Dwóch Liczb To:" << a + b << endl; // Wypisuje wynik dzialania (Sumy)
+
+    return 0 ;
 }
